1f5503a05ae03c9172344ffe590d7d89b44aeaa9
chrlesdev <charlescornelio0813@gmail.com>
2026-01-22 15:15:06 +0700

    fixed: seed.ts

--- api/prisma/seed.ts
+++ api/prisma/seed.ts
@@ -3,9 +3,12 @@ import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient, Prisma } from "../src/generated/prisma/client";
 
 const adapter = new PrismaMariaDb({
-  host: "localhost",
+  host: process.env.DATABASE_HOST || "localhost",
   port: 3306,
   connectionLimit: 5,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
 });
 
 const prisma = new PrismaClient({ adapter });
